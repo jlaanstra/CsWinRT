6b7a34e2f2694436d17aa59437dbd40b6bd93794
Joshua Larkin <70237359+j0shuams@users.noreply.github.com>
2021-06-03 10:46:21 -0700

    Add benchmark to test b/t QI and just constructing. Uncomment some benchmarks.

--- src/Benchmarks/ReflectionPerf.cs
+++ src/Benchmarks/ReflectionPerf.cs
@@ -12,61 +12,61 @@ namespace Benchmarks
         [GlobalSetup]
         public void Setup()
         {
-            //TestObject = new Class();
+            TestObject = new Class();
             instance = new ClassWithMarshalingRoutines();
         }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForNewKeyValuePair()
-        //{
-        //    return instance.NewTypeErasedKeyValuePairObject;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForNewKeyValuePair()
+        {
+           return instance.NewTypeErasedKeyValuePairObject;
+        }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForNewArray()
-        //{
-        //    return instance.NewTypeErasedArrayObject;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForNewArray()
+        {
+           return instance.NewTypeErasedArrayObject;
+        }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForNewNullable()
-        //{
-        //    return instance.NewTypeErasedNullableObject;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForNewNullable()
+        {
+           return instance.NewTypeErasedNullableObject;
+        }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForExistingKeyvaluePair()
-        //{
-        //    return instance.ExistingTypeErasedKeyValuePairObject;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForExistingKeyvaluePair()
+        {
+           return instance.ExistingTypeErasedKeyValuePairObject;
+        }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForExistingArray()
-        //{
-        //    return instance.ExistingTypeErasedArrayObject;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForExistingArray()
+        {
+           return instance.ExistingTypeErasedArrayObject;
+        }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForExistingNullable()
-        //{
-        //    return instance.ExistingTypeErasedNullableObject;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForExistingNullable()
+        {
+           return instance.ExistingTypeErasedNullableObject;
+        }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForString()
-        //{
-        //    return instance.DefaultStringProperty;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForString()
+        {
+           return instance.DefaultStringProperty;
+        }
 
-        //[Benchmark]
-        //public object ExecuteMarshalingForCustomObject()
-        //{
-        //    return instance.NewWrappedClassObject;
-        //}
+        [Benchmark]
+        public object ExecuteMarshalingForCustomObject()
+        {
+           return instance.NewWrappedClassObject;
+        }
 
-        //[Benchmark]
-        //public void StringEventSource()
-        //{
+        // [Benchmark]
+        // public void StringEventSource()
+        // {
         //    string test_string = "x";
         //    string test_string2 = "y";
 
@@ -82,7 +82,7 @@ namespace Benchmarks
         //    // In hstring from native->managed only creates System.String on demand
         //    TestObject.StringPropertyChanged += (Class sender, string value) => sender.StringProperty2 = value;
         //    TestObject.RaiseStringChanged();
-        //}
+        // }
 
         [Benchmark]
         public void IntEventSource()
