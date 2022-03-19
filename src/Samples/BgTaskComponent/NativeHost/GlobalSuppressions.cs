cd116cbc3fd93c4f7295972c454de1c610a3da59
Johan Laanstra <jlaans@microsoft.com>
2022-03-18 19:47:45 -0700

    Make self-contained work.

---
+++ src/Samples/BgTaskComponent/NativeHost/GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Minor Code Smell", "S3400:Methods should not return constants", Justification = "PInvokes.", Scope = "member", Target = "~M:WinRT.Host.Shim.Exports.DllCanUnloadNow~System.Int32")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "PInvokes.", Scope = "member", Target = "~F:WinRT.Host.Shim.Exports.S_OK")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "PInvokes.", Scope = "member", Target = "~F:WinRT.Host.Shim.Exports.S_FALSE")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "PInvokes.", Scope = "member", Target = "~F:WinRT.Host.Shim.Exports.E_NOINTERFACE")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "PInvokes.", Scope = "member", Target = "~F:WinRT.Host.Shim.Exports.REGDB_E_READREGDB")]
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names should not contain underscore", Justification = "PInvokes.", Scope = "member", Target = "~F:WinRT.Host.Shim.Exports.REGDB_E_CLASSNOTREG")]
